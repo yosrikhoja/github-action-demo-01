f0a39e2202ea0b978d62b609ab472c9c55016f9c
yosrikhoja <khojayosri@gmail.com>
2025-05-06 11:23:27 +0100

    my first commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+provider "aws" {
+  region  = "us-east-1"         # or your preferred region
+  profile = "super_user"           # only if you are using named profiles
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners =      ["099720109477"] # Canonical
+}
+
+resource    "aws_instance"      "web1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
